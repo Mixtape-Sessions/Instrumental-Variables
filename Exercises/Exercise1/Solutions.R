6860f7f231e48c68af87f5cc135c4d6fa48c8648
Kyle Butts <buttskyle96@gmail.com>
2025-02-12 17:07:06 -0600

    upload correct solutions

--- Exercises/Exercise1/Solutions.R
+++ Exercises/Exercise1/Solutions.R
@@ -1,87 +1,111 @@
-## Example Solutions for Exercise 2 --------------------------------------------
+## Example Solutions for Exercise 1 --------------------------------------------
 ## Kyle Butts, CU Boulder Economics
 ##
 ## Solutions for Peter Hull's Instrumental Variables Mixtape Session
 
+library(haven) # Read .dta files
+library(data.table) # For working with data
+library(fixest) # For regressions
+library(binsreg) # For binscatter
+library(ggplot2)
 
-library(data.table)
-library(haven)
-library(fixest)
-library(SteinIV)
+## Load data
+# data <- haven::read_dta("https://github.com/Mixtape-Sessions/Instrumental-Variables/blob/main/Exercises/Exercise1/angrist_krueger_91.dta?raw=true")
+data <- read_dta("~/Downloads/angrist_krueger_91.dta")
+data <- as.data.table(data)
 
-# ---- Setup Data --------------------------------------------------------------
-df <- read_dta("https://github.com/Mixtape-Sessions/Instrumental-Variables/blob/main/Data/stevenson.dta?raw=true")
-setDT(df)
-df <- df[black == 1, ]
-
-# ---- Get dummy IV ------------------------------------------------------------
-est <- feols(jail3 ~ judge_pre_.[1:7], df)
-df[, leniency := predict(est)]
-df[, more_lenient := (leniency > median(leniency))]
-
-# ---- Naive OLS and 2SLS ------------------------------------------------------
-feols(guilt ~ jail3, df, vcov="hc1")
-feols(guilt ~ 1 | 0 | jail3 ~ i(more_lenient), df, vcov="hc1")
+data[, qob_1 := (qob == 1)]
+data[, qob_2 := (qob == 2)]
+data[, qob_3 := (qob == 3)]
+data[, qob_4 := (qob == 4)]
 
 
-# ---- Balance on prior felonies -----------------------------------------------
-feols(prior_felChar ~ i(more_lenient), df, vcov="hc1")
-feols(prior_felChar ~ i(more_lenient) | bailDate, df, vcov="hc1")
 
-# ---- Controlled OLS and 2SLS -------------------------------------------------
-feols(guilt ~ jail3 | bailDate, df, vcov="hc1")
-feols(guilt ~ 1 | bailDate | jail3 ~ more_lenient, df, vcov="hc1")
+# ---- OLS and Binscatter ------------------------------------------------------
 
-# ---- Estimate complier Y0 and compare to overall E[Y|D=0] --------------------
-df[,
-	 Y_omD := guilt * (1 - jail3)
-][,
-	omD := (1 - jail3)
+feols(
+	lwage ~ educ, # Regression formula
+	data,
+	vcov = "hc1" # ,r
+)
+
+binscatter <- binsreg(data$lwage, data$educ)
+
+# plot and add labels
+binscatter$bins_plot +
+	labs(y = "Log wages", x = "Years of Completed Schooling")
+
+
+
+# ---- Simple (Wald) IV Estimator ----------------------------------------------
+
+# Formula y ~ exogenous | fixed effects | endogenous ~ instrument
+# 1 = constant, 0 = no fixed effects
+feols(
+	lwage ~ 1 | 0 | educ ~ qob_1,
+	data,
+	vcov = "hc1"
+)
+
+
+data[,
+		 .(n = .N, mean = mean(lwage), sd = sd(lwage), min = min(lwage), max = max(lwage)),
+		 by = qob_1
+]
+data[,
+		 .(n = .N, mean = mean(educ), sd = sd(educ), min = min(educ), max = max(educ)),
+		 by = qob_1
 ]
 
-feols(Y_omD ~ 1 | bailDate | omD ~ i(more_lenient), df, vcov="hc1")
-
-summary(df[jail3 == 0, "guilt"])
-
-# ---- 2SLS using all judges ---------------------------------------------------
-feols(guilt ~ 1 | 0 | jail3 ~ judge_pre_.[1:7], df, vcov="hc1")
-feols(guilt ~ 1 | 0 | jail3 ~ leniency, df, vcov="hc1")
 
 
-# ---- JIVE using all judges ---------------------------------------------------
+# ---- Overidentified IV Estimator ---------------------------------------------
 
-jive.est(
-	y = df$guilt, X = as.matrix(df[,.(intercept = rep(1, .N), jail3)]),
-	Z = as.matrix(df[,.SD, .SDcols=patterns("judge_pre_")])
+feols(
+	lwage ~ 1 | 0 | educ ~ qob_1 + qob_2 + qob_3,
+	data,
+	vcov = "hc1"
 )
 
-# Make judge ID
-df[, judge := (judge_pre_1 == 1) * 1 +
-	 	(judge_pre_2 == 1) * 2 +
-	 	(judge_pre_3 == 1) * 3 +
-	 	(judge_pre_4 == 1) * 4 +
-	 	(judge_pre_5 == 1) * 5 +
-	 	(judge_pre_6 == 1) * 6 +
-	 	(judge_pre_7 == 1) * 7 +
-	 	(judge_pre_8 == 1) * 8]
+# collapse data by qob
+collapsed <- data[,
+		 .(lwage = mean(lwage), educ = mean(educ)),
+		 by = qob
+]
 
-df[, num := .N, by = judge]
-df[, lo_leniency := (leniency*num - jail3)/(num - 1)]
+# plot means
+plot(collapsed$educ, collapsed$lwage)
 
-feols(guilt ~ 1 | 0 | jail3 ~ lo_leniency, df, vcov="hc1")
+# add regression line
+abline(feols(lwage ~ educ, collapsed))
 
 
-# ---- 2SLS / JIVE / UJIVE using all judges  & controls ------------------------
 
-feols(guilt ~ 1 | bailDate | jail3 ~ judge_pre_.[1:7], df, vcov="hc1")
+# ---- Putting the 2S in 2SLS --------------------------------------------------
 
-# Jonathan Seward ported UJIVE to R
-# Can't use bailDate fixed effects since the matrix is too large to invert
-source("https://raw.githubusercontent.com/j-seward/ujive/main/ivreg_001.R")
-
-ivreg(
-	y = df$guilt, T = df$jail3,
-	Z = as.matrix(df[,.SD, .SDcols=patterns("judge_pre_")]),
-	W = as.matrix(df[, .(rep(1, .N))]), # Constant only
-	noConstant = FALSE
+feols(
+	lwage ~ 1 | yob | educ ~ qob_1 + qob_2 + qob_3,
+	data,
+	vcov = "hc1"
 )
+
+first_stage <- feols(educ ~ i(qob) | yob, data)
+
+data[, educ_hat := predict(first_stage)]
+
+feols(
+	lwage ~ educ_hat | yob,
+	data,
+	vcov = "hc1"
+)
+
+
+
+# ---- Many IV Bias ------------------------------------------------------------
+
+feols(
+	lwage ~ 1 | yob | educ ~ i(yob, qob_1) + i(yob, qob_2) + i(yob, qob_3),
+	data,
+	vcov = "hc1"
+)
+
